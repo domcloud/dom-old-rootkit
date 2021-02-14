4c7fa02de4431fcb505b819d7a2b295cd25a3865
Wildan M <willnode@wellosoft.net>
2021-02-14 09:06:45 +0700

    Too slow! Use the mighty regex

--- index.php
+++ index.php
@@ -1,15 +1,7 @@
 <?php
 
-require "vendor/autoload.php";
+require_once "vendor/autoload.php";
 require "validator.php";
-
-use Madkom\NginxConfigurator\Builder;
-use Madkom\NginxConfigurator\Config\Location;
-use Madkom\NginxConfigurator\Config\Server;
-use Madkom\NginxConfigurator\Node\Directive;
-use Madkom\NginxConfigurator\Node\Node;
-use Madkom\NginxConfigurator\Node\Param;
-use Madkom\NginxConfigurator\Parser;
 
 $dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
 $dotenv->load();
@@ -23,118 +15,72 @@ if (!$nginx_file) {
     die('ERROR: config not found');
 }
 
-/**
- * @param Node $server
- * @return Location[]
- * */
-function extractLocations($server)
-{
-    return iterator_to_array($server->search(function ($x) {
-        return $x instanceof Location;
-    }));
-}
-
-/**
- * @param Node $server
- * @return Directive[]
- * */
-function extractDirective($server, $directive)
-{
-    return iterator_to_array($server->search(function ($x) use ($directive) {
-        return $x instanceof Directive && $x->getName() === $directive;
-    }));
-}
-/**
- * @param Directive $directive
- * @return mixed[]
- * */
-function extractParameters($directive)
-{
-    return array_map(function ($x) {
-        return $x instanceof Param ? $x->getValue() : null;
-    }, iterator_to_array($directive->getParams()));
-}
-
-
-// parse config files
 $found = false;
 $target = $_GET['domain'];
-$nginx_builder = new Builder();
-$nginx_head = explode("server {", $nginx_file, 2);
-$nginx_body = "server {" . trim($nginx_head[1]);
-$nginx_body = substr($nginx_body, 0, -1);
-$nginx_foot = "\n}";
-$nginx_head = $nginx_head[0];
-$nginx_conf = (new Parser())->parse(str_replace("\t", '', $nginx_body));
-foreach ($nginx_conf->getIterator() as $n) {
-    if ($n instanceof Server) {
-        $dir = $n->search(function ($node) use ($target) {
-            if ($node instanceof Directive && $node->getName() === 'server_name') {
-                foreach ($node->getParams() as $p) {
-                    if ($p instanceof Param && $p->getValue() === $target) {
-                        return true;
-                    }
-                }
-            }
-            return false;
-        });
-        if ($dir->count()) {
-            // is this GET or POST?
-            if ($_SERVER['REQUEST_METHOD'] === 'GET') {
-                $nginx_builder = new Builder();
-                $nginx_builder->appendServerNode($n);
-                echo $nginx_builder->dump();
-            }
-            if ($_SERVER['REQUEST_METHOD'] !== 'POST') {
-                return;
-            }
-            // extract data
-            $d['dom'] = $target;
-            foreach (extractDirective($n, 'listen') as $ips) {
-                $ip = str_replace(':443', '', extractParameters($ips)[0]);
-                $d[$ip[0] === '[' ? 'ip6' : 'ip'] = $ip;
-            }
-            $d['root'] = extractParameters(extractDirective($n, 'root')[0])[0];
-            $d['index'] = extractParameters(extractDirective($n, 'index')[0])[0];
-            $d['access_log'] = extractParameters(extractDirective($n, 'access_log')[0])[0];
-            $d['error_log'] = extractParameters(extractDirective($n, 'error_log')[0])[0];
-            $ssl = extractDirective($n, 'ssl_certificate');
-            if (count($ssl)) {
-                $d['ssl']['cert'] = extractParameters($ssl[0])[0];
-                $d['ssl']['key'] = extractParameters(extractDirective($n, 'ssl_certificate_key')[0])[0];
-            }
-            // find the location who hass fcgi
-            foreach (extractLocations($n) as $l) {
-                if (count($fcgi = extractDirective($l, 'fastcgi_pass'))) {
-                    $d['fcgi'] = extractParameters($fcgi[0])[0];
-                    break;
-                }
-            }
-            // process config
-            $c = mergeConfig(file_get_contents('php://input'));
-            $c['locations'][] = [
-                'match' => '~ \.php(/|$)',
-                'try_files' => '$uri =404',
-                'fastcgi_pass' => $d['fcgi'],
-            ];
-            // all necessary data in, now cut
-            ob_start();
-            include "template.php";
-            $file = str_replace("\t", "", ob_get_clean());
-            $nginx_builder->appendServerNode(iterator_to_array((new Parser())->parse($file)->getIterator())[0]);
-            $found = true;
-        } else {
-            $nginx_builder->appendServerNode($n);
-        }
-    }
-}
 
-if (!$found) {
+// normalize config files
+$nginx_file = str_replace("\r", "", $nginx_file);
+$nginx_file = str_replace('    ', "\t", $nginx_file);
+$matches = [];
+if (!preg_match('/\n	server {\n\t\tserver_name ' . str_replace('.', '\.', $target) . '.+?\n\t}/s', $nginx_file, $matches)) {
     die('ERROR: domain not found');
 }
+$serv = $matches[0];
+$serv_substart = strpos($nginx_file, $serv);
+$serv_subend = $serv_substart + strlen($serv);
+
+// is this GET or POST?
+if ($_SERVER['REQUEST_METHOD'] === 'GET') {
+    echo preg_replace('/^\t/m', "", substr($serv, 1));
+}
+if ($_SERVER['REQUEST_METHOD'] !== 'POST') {
+    return;
+}
+
+// extract data
+$d['dom'] = $target;
+$matches = [];
+if (preg_match_all('/^\t\tlisten (.+?)( ssl)?( http2)?;/m', $serv, $matches) === false) {
+    die('ERROR: No "listen" was detected');
+}
+foreach ($matches[1] as $ips) {
+    $ip = str_replace(':443', '', $ips);
+    $d[$ip[0] === '[' ? 'ip6' : 'ip'] = $ip;
+}
+foreach (['root', 'index', 'access_log', 'error_log'] as $variable) {
+    $matches = [];
+    if (preg_match('/^\t\t' . $variable . ' (.+);/m', $serv, $matches) === false) {
+        die("ERROR: No '$variable' was detected");
+    }
+    $d[$variable] = $matches[1];
+}
+$matches = [];
+if (preg_match('/^\t\tssl_certificate (.+);/m', $serv, $matches) !== false) {
+    $d['ssl']['cert'] = $matches[1];
+    $matches = [];
+    preg_match('/^\t\tssl_certificate_key (.+);/m', $serv, $matches);
+    $d['ssl']['key'] = $matches[1];
+}
+// extract location
+$matches = [];
+if (preg_match('/^\t\t\tfastcgi_pass (.+);/m', $serv, $matches) === false) {
+    die("ERROR: No 'fastcgi_pass' was detected");
+}
+$d['fcgi'] = $matches[1];
+$c = mergeConfig($_POST['data'] ?? file_get_contents('php://input'));
+$c['locations'][] = [
+    'match' => '~ \.php(/|$)',
+    'try_files' => '$uri =404',
+    'fastcgi_pass' => $d['fcgi'],
+];
+// all necessary data in, now cut
+ob_start();
+include "template.php";
+$serv = ob_get_clean();
+
+
 // dump
-$nginx_body = str_replace("\n", "\n\t", trim($nginx_builder->dump()));
-$nginx_new = $nginx_head . $nginx_body . $nginx_foot;
+$nginx_new = substr($nginx_file, 0, $serv_substart) . $serv . substr($nginx_file, $serv_subend);
 
 if (($_GET['preview'] ?? '') === 'all') {
     die($nginx_new);
--- template.php
+++ template.php
@@ -1,71 +1,72 @@
-server {
-	server_name <?= $d['dom'] ?>;
-	<?php if ($c['ssl'] !== 'enforce') : ?>
-	listen <?= $d['ip'] ?>;
-	listen <?= $d['ip6'] ?>;
-	<?php endif ?>
-	root <?= $d['root'] ?>;
-	index <?= $c['index'] ?>;
-	access_log <?= $d['access_log'] ?>;
-	error_log <?= $d['error_log'] ?>;
-	<?php if (!empty($c['gzip'])) : ?>
-	gzip on;
-	<?php
-	foreach ([
-		'types', 'min_length', 'proxied',
-	] as $key) {
-		if (!empty($c['gzip'][$key])) {
-			echo "\tgzip_$key ".$c['gzip'][$key].";\n";
-		}
+
+	server {
+		server_name <?= $d['dom'] ?>;
+<?php if ($c['ssl'] !== 'enforce') : ?>
+		listen <?= $d['ip'] ?>;
+		listen <?= $d['ip6'] ?>;
+<?php endif ?>
+		root <?= $d['root'] ?>;
+		index <?= $c['index'] ?>;
+		access_log <?= $d['access_log'] ?>;
+		error_log <?= $d['error_log'] ?>;
+<?php if (!empty($c['gzip'])) : ?>
+		gzip on;
+<?php
+foreach ([
+	'types', 'min_length', 'proxied',
+] as $key) {
+	if (!empty($c['gzip'][$key])) {
+		echo "\t\tgzip_$key ".$c['gzip'][$key].";\n";
 	}
-	?>
-	<?php endif ?>
-	<?php if (!empty($c['passenger'])) {
-		foreach ([
-			'enabled', 'app_env', 'app_type',
-			'startup_file', 'ruby', 'nodejs', 'python',
-			'meteor_app_settings', 'friendly_error_pages',
-		] as $key) {
-			if (!empty($c['passenger'][$key])) {
-				echo "\tpassenger_$key ".$c['passenger'][$key].";\n";
-			}
-		}
-		foreach (($c['passenger']['env_vars'] ?? []) as $env) {
-			echo "\tpassenger_env_var ".$env.";\n";
-		}
-		if (!empty($c['passenger']['app_start_command'])) {
-			echo "\tpassenger_app_start_command ".escapeshellarg($c['passenger']['app_start_command']).";\n";
-		}
-	} ?>
-    <?php foreach ($c['error_pages'] as $e) : ?>
-    error_page <?= $e ?>;
-    <?php endforeach ?>
-    <?php foreach ($c['locations'] as $l) : ?>
-    location <?= $l['match'] ?> {
-		<?php
-		foreach ([
-			'try_files', 'fastcgi_pass', 'return',
-		] as $key) {
-			if (isset($l[$key])) {
-				echo "\t$key ".$l[$key].";\n";
-			}
-		}
-		foreach ([
-			'root', 'alias',
-		] as $key) {
-			if (isset($l[$key])) {
-				echo "\t$key ".$d['root'].'/'.trim(str_replace('..', '', $l[$key]), '/').";\n";
-			}
-		}
-		?>
-	}
-    <?php endforeach ?>
-	<?php if ($c['ssl'] !== 'off') : ?>
-	listen <?= $d['ip'] ?>:443 ssl http2;
-	listen <?= $d['ip6'] ?>:443 ssl http2;
-    <?php endif ?>
-    <?php if (isset($d['ssl'])) : ?>
-	ssl_certificate <?= $d['ssl']['cert'] ?>;
-	ssl_certificate_key <?= $d['ssl']['key'] ?>;
-    <?php endif ?>
 }
+?>
+<?php endif ?>
+<?php if (!empty($c['passenger'])) {
+	foreach ([
+		'enabled', 'app_env', 'app_type',
+		'startup_file', 'ruby', 'nodejs', 'python',
+		'meteor_app_settings', 'friendly_error_pages',
+	] as $key) {
+		if (!empty($c['passenger'][$key])) {
+			echo "\tpassenger_$key ".$c['passenger'][$key].";\n";
+		}
+	}
+	foreach (($c['passenger']['env_vars'] ?? []) as $env) {
+		echo "\tpassenger_env_var ".$env.";\n";
+	}
+	if (!empty($c['passenger']['app_start_command'])) {
+		echo "\tpassenger_app_start_command ".escapeshellarg($c['passenger']['app_start_command']).";\n";
+	}
+} ?>
+<?php foreach ($c['error_pages'] as $e) : ?>
+		error_page <?= $e ?>;
+<?php endforeach ?>
+<?php foreach ($c['locations'] as $l) : ?>
+		location <?= $l['match'] ?> {
+<?php
+foreach ([
+	'try_files', 'fastcgi_pass', 'return',
+] as $key) {
+	if (isset($l[$key])) {
+		echo "\t\t\t$key ".$l[$key].";\n";
+	}
+}
+foreach ([
+	'root', 'alias',
+] as $key) {
+	if (isset($l[$key])) {
+		echo "\t\t\t$key ".$d['root'].'/'.trim(str_replace('..', '', $l[$key]), '/').";\n";
+	}
+}
+?>
+		}
+<?php endforeach ?>
+<?php if ($c['ssl'] !== 'off') : ?>
+		listen <?= $d['ip'] ?>:443 ssl http2;
+		listen <?= $d['ip6'] ?>:443 ssl http2;
+<?php endif ?>
+<?php if (isset($d['ssl'])) : ?>
+		ssl_certificate <?= $d['ssl']['cert'] ?>;
+		ssl_certificate_key <?= $d['ssl']['key'] ?>;
+<?php endif ?>
+	}
