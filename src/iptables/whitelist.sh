01fb81eac0426b37efcc03f72ad8afe48fa56f87
Wildan M <willnode@wellosoft.net>
2021-06-04 03:47:28 +0700

    Dont flush ipset

--- src/iptables/whitelist.sh
+++ src/iptables/whitelist.sh
@@ -2,10 +2,12 @@
 
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 
+### Create Ipset
 # ipset create whitelist hash:ip
-# ipset flush whitelist-v6 hash:ip inet6
-ipset flush whitelist
-ipset flush whitelist-v6
+# ipset create whitelist-v6 hash:ip family inet6
+### Clear Ipset, not gonna use it because DNS IPs often changing
+# ipset flush whitelist
+# ipset flush whitelist-v6
 while read p; do
   if [[ $p != "#"* ]];
   then
